c748afe5e2cef8e12343fa29321e6faae8875ca7
Mukund Chaurasiya <achaurasiya470mukund@gmail.com>
2026-05-03 14:30:13 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # CONFIG
 # =============================
 # Change to your deployed URL or keep localhost for local dev
-API_BASE = "http://127.0.0.1:8000"
+API_BASE = "https://movie-recommender-vq66.onrender.com"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
